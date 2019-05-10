c975da9d7e334d31f7a4817d4408a7b8dfa25c24
Andres Huertas <andreshcl29@gmail.com>
2019-05-10 17:58:24 -0500

    03 rutas

---
+++ 03-Rutas/rutas/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RutaHomeComponent } from './rutas/ruta-home/ruta-home.component';
+import { RutaCreditosComponent } from './rutas/ruta-creditos/ruta-creditos.component';
+import { RutaProductosComponent } from './rutas/ruta-productos/ruta-productos.component';
+import { RutaNoEncontradaComponent } from './rutas/ruta-no-encontrada/ruta-no-encontrada.component';
+import { RutaProductosHogarComponent } from './rutas/ruta-productos-hogar/ruta-productos-hogar.component';
+import { RutaProductosVideojuegosComponent } from './rutas/ruta-productos-videojuegos/ruta-productos-videojuegos.component';
+import { RutaProductosVideojuegosMarvelComponent } from './rutas/ruta-productos-videojuegos-marvel/ruta-productos-videojuegos-marvel.component';
+import { RutaProductosVideojuegosAccionComponent } from './rutas/ruta-productos-videojuegos-accion/ruta-productos-videojuegos-accion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RutaHomeComponent,
+    RutaCreditosComponent,
+    RutaProductosComponent,
+    RutaNoEncontradaComponent,
+    RutaProductosHogarComponent,
+    RutaProductosVideojuegosComponent,
+    RutaProductosVideojuegosMarvelComponent,
+    RutaProductosVideojuegosAccionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 03-Rutas/rutas/src/app/rutas/ruta-productos-videojuegos-marvel/ruta-productos-videojuegos-marvel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ruta-productos-videojuegos-marvel',
+  templateUrl: './ruta-productos-videojuegos-marvel.component.html',
+  styleUrls: ['./ruta-productos-videojuegos-marvel.component.css']
+})
+export class RutaProductosVideojuegosMarvelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
